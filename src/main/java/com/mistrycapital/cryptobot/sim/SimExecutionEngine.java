5f3585f78e5d824eb699d23e7d6ed9f0f3f545a8
Abhishek Mistry <mail@mistry.info>
2018-04-16 23:09:21 -0400

    Adjust parameters and fix execution borrow bug

--- src/main/java/com/mistrycapital/cryptobot/sim/SimExecutionEngine.java
+++ src/main/java/com/mistrycapital/cryptobot/sim/SimExecutionEngine.java
@@ -7,11 +7,15 @@ import com.mistrycapital.cryptobot.execution.TradeInstruction;
 import com.mistrycapital.cryptobot.gdax.common.Currency;
 import com.mistrycapital.cryptobot.gdax.common.OrderSide;
 import com.mistrycapital.cryptobot.gdax.common.Product;
+import com.mistrycapital.cryptobot.util.MCLoggerFactory;
 import com.mistrycapital.cryptobot.util.MCProperties;
+import org.slf4j.Logger;
 
 import java.util.List;
 
 public class SimExecutionEngine implements ExecutionEngine {
+	private static final Logger log = MCLoggerFactory.getLogger();
+
 	private final Accountant accountant;
 	private final double transactionCost;
 	private ConsolidatedSnapshot snapshot;
@@ -32,13 +36,14 @@ public class SimExecutionEngine implements ExecutionEngine {
 			final Product product = instruction.getProduct();
 			final Currency cryptoCurrency = product.getCryptoCurrency();
 
+			// make sure not to trade more than we have available, regardless of the instructions
 			if(instruction.getOrderSide() == OrderSide.BUY) {
-				final double crypto = instruction.getAmount() * (1 - transactionCost);
 				final double price = snapshot.getProductSnapshot(product).askPrice;
-				final double dollars = instruction.getAmount() * price;
+				final double dollars = Math.min(instruction.getAmount() * price, accountant.getAvailable(Currency.USD));
+				final double crypto = dollars * (1 - transactionCost) / price;
 				accountant.recordTrade(Currency.USD, -dollars, cryptoCurrency, crypto);
 			} else {
-				final double crypto = instruction.getAmount();
+				final double crypto = Math.min(instruction.getAmount(), accountant.getAvailable(cryptoCurrency));
 				final double price = snapshot.getProductSnapshot(product).bidPrice;
 				final double dollars = (1 - transactionCost) * crypto * price;
 				accountant.recordTrade(Currency.USD, dollars, cryptoCurrency, -crypto);
