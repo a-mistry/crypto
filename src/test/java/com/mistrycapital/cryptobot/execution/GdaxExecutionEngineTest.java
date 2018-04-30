caa059373aa8a744868d46cc90d58353965d884a
Abhishek Mistry <mail@mistry.info>
2018-04-30 07:59:30 -0400

    Complete execution test but not working

--- src/test/java/com/mistrycapital/cryptobot/execution/GdaxExecutionEngineTest.java
+++ src/test/java/com/mistrycapital/cryptobot/execution/GdaxExecutionEngineTest.java
@@ -8,14 +8,17 @@ import com.mistrycapital.cryptobot.book.OrderBookManager;
 import com.mistrycapital.cryptobot.gdax.client.GdaxClient;
 import com.mistrycapital.cryptobot.gdax.client.MarketOrderSizingType;
 import com.mistrycapital.cryptobot.gdax.client.OrderInfo;
+import com.mistrycapital.cryptobot.gdax.client.OrderStatus;
 import com.mistrycapital.cryptobot.gdax.common.Currency;
 import com.mistrycapital.cryptobot.gdax.common.OrderSide;
 import com.mistrycapital.cryptobot.gdax.common.Product;
+import com.mistrycapital.cryptobot.gdax.common.Reason;
 import com.mistrycapital.cryptobot.sim.SimTimeKeeper;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
 import java.util.Arrays;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,6 +31,7 @@ class GdaxExecutionEngineTest {
 		throws Exception
 	{
 		final SimTimeKeeper timeKeeper = new SimTimeKeeper();
+		timeKeeper.advanceTime(System.currentTimeMillis()*1000000L);
 		final OrderBookManager orderBookManager = mock(OrderBookManager.class);
 		final OrderBook btcBook = mock(OrderBook.class);
 		final Accountant accountant = mock(Accountant.class);
@@ -44,9 +48,22 @@ class GdaxExecutionEngineTest {
 
 		final double fillFees = 10.0 * 0.0030;
 		final double executedValue = 10.0 - fillFees;
-		final double fillSize = executedValue / 10000.0;
+		final double filledSize = executedValue / 10000.0;
 		JsonObject json = new JsonObject();
-		fail("need to add json data");
+		json.addProperty("id", UUID.randomUUID().toString());
+		json.addProperty("product_id", Product.BTC_USD.toString());
+		json.addProperty("side","buy");
+		json.addProperty("type","market");
+		json.addProperty("created_at", timeKeeper.iso8601());
+		json.addProperty("fillFees", Double.toString(fillFees));
+		json.addProperty("filledSize", Double.toString(filledSize));
+		json.addProperty("executedValue", Double.toString(executedValue));
+		json.addProperty("staus", OrderStatus.DONE.toString().toLowerCase());
+		json.addProperty("settled", true);
+		json.addProperty("funds", "10.0");
+		json.addProperty("specifiedFunds", "10.0");
+		json.addProperty("done_at", timeKeeper.iso8601());
+		json.addProperty("done_reason", Reason.FILLED.toString().toLowerCase());
 		when(gdaxClient.placeMarketOrder(Product.BTC_USD, OrderSide.BUY, MarketOrderSizingType.FUNDS, 10.0))
 			.thenReturn(CompletableFuture.supplyAsync(() -> new OrderInfo(json)));
 
@@ -58,6 +75,6 @@ class GdaxExecutionEngineTest {
 		ArgumentCaptor<Double> cryptoArg = ArgumentCaptor.forClass(Double.class);
 		verify(accountant, times(1)).recordTrade(same(Currency.USD), usdArg.capture(), same(Currency.BTC), cryptoArg.capture());
 		assertEquals(-executedValue-fillFees, usdArg.getValue().doubleValue());
-		assertEquals(fillSize, cryptoArg.getValue().doubleValue());
+		assertEquals(filledSize, cryptoArg.getValue().doubleValue());
 	}
 }
